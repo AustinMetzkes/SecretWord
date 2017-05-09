713ec8720abbf58de795684d3d2217430dc1628b
AustinMetzkes <austin.metzkes@students.fhu.edu>
2017-05-09 15:30:06 -0500

    start

--- SecretWord/Controllers/AccountController.cs
+++ SecretWord/Controllers/AccountController.cs
@@ -214,7 +214,7 @@ namespace SecretWord.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
                 var result = await _userManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
